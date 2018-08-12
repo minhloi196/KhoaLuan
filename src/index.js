78bd8dde1018f84965e43c625ee9ac2c859423c7
Minh Loi <nguyenminhloi196@gmail.com>
2018-08-12 12:25:21 +0700

    Create Web BI

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import './polyfill'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+// disable ServiceWorker
+// import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+// disable ServiceWorker
+// registerServiceWorker();
