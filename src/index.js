a73609e8451a02deb48516c9be847e04a205419b
Phan Thanh Huy <phanthanhhuy1996@gmail.com>
2018-08-12 13:29:58 +0700

    Delete unused features in UI

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 // disable ServiceWorker
 // import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    // <Provider store={store}>
+        <App />,
+    // </Provider>,
     document.getElementById('root')
 );
 // disable ServiceWorker
