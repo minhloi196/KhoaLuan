46241d673515321c06e77c30679020620ec9c167
Phan Thanh Huy <phanthanhhuy1996@gmail.com>
2018-10-10 23:21:48 +0700

    Test deploy 2

--- src/views/Charts/Charts.js
+++ src/views/Charts/Charts.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
-import { Card, CardBody, CardColumns, CardHeader, Button } from 'reactstrap';
-import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
-import EChartLine from './EChartLine';
+// import React, { Component } from 'react';
+// import { Card, CardBody, CardColumns, CardHeader, Button } from 'reactstrap';
+// import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+// import EChartLine from './EChartLine';
 
 
 
-class Charts extends Component {
-  render() {
-    return (
-      <div className="animated fadeIn">
-        {/* <CardColumns className="cols-2"> */}
-          <Card>
-            <CardHeader>
-              Line Chart
-              <div className="card-header-actions">
-                {/* <Button color="primary" className="float-right"><i className="icon-cloud-download"></i></Button> */}
-                {/* <span style={{width: '50px', backgroundColor: 'pink'}}><i className="icon-cloud-download"></i></span> */}
-              </div>
-            </CardHeader>
-            <CardBody>
-              <div className="chart-wrapper">
-                <EChartLine/>
-              </div>
-            </CardBody>
-          </Card>
-        {/* </CardColumns> */}
-      </div>
-    );
-  }
-}
+// class Charts extends Component {
+//   render() {
+//     return (
+//       <div className="animated fadeIn">
+//         {/* <CardColumns className="cols-2"> */}
+//           <Card>
+//             <CardHeader>
+//               Line Chart
+//               <div className="card-header-actions">
+//                 {/* <Button color="primary" className="float-right"><i className="icon-cloud-download"></i></Button> */}
+//                 {/* <span style={{width: '50px', backgroundColor: 'pink'}}><i className="icon-cloud-download"></i></span> */}
+//               </div>
+//             </CardHeader>
+//             <CardBody>
+//               <div className="chart-wrapper">
+//                 <EChartLine/>
+//               </div>
+//             </CardBody>
+//           </Card>
+//         {/* </CardColumns> */}
+//       </div>
+//     );
+//   }
+// }
 
-export default Charts;
+// export default Charts;
